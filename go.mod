a48ac2bc1f1a57c87fb30b245677b6a940f9dbfc
Wei Xing <weixingp5@gmail.com>
2022-09-07 17:41:47 +0800

    add package definition for fs

--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
 module CZ4031-Project1
 
 go 1.19
+
+require internal/fs v1.0.0
+replace internal/fs => ./internal/fs
---
+++ internal/fs/fs.go
@@ -0,0 +1,9 @@
+// Package fs contains implementation for in-memory file system
+package fs
+
+import "fmt"
+
+// Fs prints "Hello from Bar"
+func Fs() {
+	fmt.Println("Hello from Fs")
+}
